3c3527a5a6fba57d3b217057abb88db78af51578
husen <husen@zdns.cn>
2022-05-26 17:48:50 +0800

    demo

---
+++ src/houseData/2022/index.js
@@ -0,0 +1,13 @@
+
+/**
+ * 区域[districtType] 中心城区 = 1 | 郊区新城 = 2
+ * 房屋类型[houseType] 新房 = 1 | 二手房 = 2
+ * */
+import march from './202203'
+import may from './202205'
+
+export default  [
+  ...march,
+  ...may
+];
+
