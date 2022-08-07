d07f03fcf042be13543852e84f848fc9002ed45e
husen <husen@zdns.cn>
2022-08-07 20:10:47 +0800

    update

--- src/houseData/2022/202208.ts
+++ src/houseData/2022/202208.ts
@@ -5,19 +5,29 @@
  * */
 
 export default [
-  { date: '2022-07-01', districtType: 1, houseType: 1, count: 275, area: 36139.55 },
-  { date: '2022-07-01', districtType: 2, houseType: 1, count: 96, area: 11007.56 },
-  { date: '2022-07-01', districtType: 1, houseType: 2, count: 595, area: 55920.54 },
-  { date: '2022-07-01', districtType: 2, houseType: 2, count: 122, area: 12417.97 },
+  { date: '2022-08-01', districtType: 1, houseType: 1, count: 275, area: 36139.55 },
+  { date: '2022-08-01', districtType: 2, houseType: 1, count: 96, area: 11007.56 },
+  { date: '2022-08-01', districtType: 1, houseType: 2, count: 595, area: 55920.54 },
+  { date: '2022-08-01', districtType: 2, houseType: 2, count: 122, area: 12417.97 },
 
-  { date: '2022-07-02', districtType: 1, houseType: 1, count: 422, area: 54650.10 },
-  { date: '2022-07-02', districtType: 2, houseType: 1, count: 85, area: 9874.45 },
-  { date: '2022-07-02', districtType: 1, houseType: 2, count: 483, area: 45116.96 },
-  { date: '2022-07-02', districtType: 2, houseType: 2, count: 93, area: 10096.46 },
+  { date: '2022-08-02', districtType: 1, houseType: 1, count: 422, area: 54650.10 },
+  { date: '2022-08-02', districtType: 2, houseType: 1, count: 85, area: 9874.45 },
+  { date: '2022-08-02', districtType: 1, houseType: 2, count: 483, area: 45116.96 },
+  { date: '2022-08-02', districtType: 2, houseType: 2, count: 93, area: 10096.46 },
 
-  { date: '2022-07-03', districtType: 1, houseType: 1, count: 896, area: 120681.09 },
-  { date: '2022-07-03', districtType: 2, houseType: 1, count: 84, area: 9040.13 },
-  { date: '2022-07-03', districtType: 1, houseType: 2, count: 479, area: 46557.51 },
-  { date: '2022-07-03', districtType: 2, houseType: 2, count: 82, area: 9147.56 },
+  { date: '2022-08-03', districtType: 1, houseType: 1, count: 896, area: 120681.09 },
+  { date: '2022-08-03', districtType: 2, houseType: 1, count: 84, area: 9040.13 },
+  { date: '2022-08-03', districtType: 1, houseType: 2, count: 479, area: 46557.51 },
+  { date: '2022-08-03', districtType: 2, houseType: 2, count: 82, area: 9147.56 },
+
+  { date: '2022-08-04', districtType: 1, houseType: 1, count: 536, area: 70709.48 },
+  { date: '2022-08-04', districtType: 2, houseType: 1, count: 92, area: 10250.43 },
+  { date: '2022-08-04', districtType: 1, houseType: 2, count: 517, area: 48080.39 },
+  { date: '2022-08-04', districtType: 2, houseType: 2, count: 75, area: 7989.4 },
+
+  { date: '2022-08-05', districtType: 1, houseType: 1, count: 546, area: 72250.34 },
+  { date: '2022-08-05', districtType: 2, houseType: 1, count: 85, area: 9616.86 },
+  { date: '2022-08-05', districtType: 1, houseType: 2, count: 496, area: 43912.21 },
+  { date: '2022-08-05', districtType: 2, houseType: 2, count: 93, area: 9096.4 },
 
 ];
