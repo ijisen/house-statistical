95ce40f22c79ebfff3073dc75cd65cbaf65b4f98
husen <husen@zdns.cn>
2022-09-01 19:32:02 +0800

    update

---
+++ src/houseData/2022/202209.ts
@@ -0,0 +1,13 @@
+/**
+ * =========  2022年 09月数据  ===========
+ * 区域[districtType] 中心城区 = 1 | 郊区新城 = 2
+ * 房屋类型[houseType] 新房 = 1 | 二手房 = 2
+ * */
+
+export default [
+  { date: '2022-09-01', districtType: 1, houseType: 1, count: 192, area: 23186.95 },
+  { date: '2022-09-01', districtType: 2, houseType: 1, count: 46, area: 5067.45 },
+  { date: '2022-09-01', districtType: 1, houseType: 2, count: 381, area: 33963.61 },
+  { date: '2022-09-01', districtType: 2, houseType: 2, count: 52, area: 6117.56 },
+
+];
