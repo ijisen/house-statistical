e8bbf40932b956361c20b638d8b75463ebfdd1f7
husen <husen@zdns.cn>
2022-10-10 16:45:35 +0800

    update

--- src/houseData/2022/202210.ts
+++ src/houseData/2022/202210.ts
@@ -10,5 +10,9 @@ export default [
   { date: '2022-10-08', districtType: 1, houseType: 2, count: 538, area: 52195.33 },
   { date: '2022-10-08', districtType: 2, houseType: 2, count: 113, area: 10678.49 },
 
+  { date: '2022-10-09', districtType: 1, houseType: 1, count: 402, area: 54052.43 },
+  { date: '2022-10-09', districtType: 2, houseType: 1, count: 64, area: 8044.27 },
+  { date: '2022-10-09', districtType: 1, houseType: 2, count: 543, area: 49734.66 },
+  { date: '2022-10-09', districtType: 2, houseType: 2, count: 89, area: 9704.29 },
 
 ];
